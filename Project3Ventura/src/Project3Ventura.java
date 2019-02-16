855c214014bec9c5be4a49d2b6244e552e0b9311
Joshua Ventura <jventura11@hawkmail.hccfl.edu>
2019-02-16 06:51:48 -0500

    Second Commit, Read Comments!

--- Project3Ventura/src/Project3Ventura.java
+++ Project3Ventura/src/Project3Ventura.java
@@ -1,8 +1,16 @@
 import java.awt.Font;
 import javax.swing.*;
+import java.awt.*;
+/*
+ * Unused imports (These may be needed later)
+ * import java.awt.Event.*; (Used for actionlistener)
+ * 
+ */
 
 public class Project3Ventura extends JFrame {
     
+	private ButtonGroup group;
+	
     public static void main(String [] args) {
         new Project3Ventura();
     }
@@ -19,58 +27,113 @@ public class Project3Ventura extends JFrame {
         //                  that shows when hovered over with the mouse)
         // setVisible - sets all components within range to show upon running
         
+    	
+        	
+        // Frame layout 
+    		this.setSize(800, 600);
+    		
+    	
+        // Sets Frame position to always start centered
+    		this.setLocationRelativeTo(null);
         
-            // Frame layout 
-	    this.setSize(800, 675);
+        // closes entire program upon exit
+    		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    		
+    		this.setTitle("Search Engine");
+    		
+    	/* I couldn't seem to get this to work below,
+    	 *	this.getContentPane().setBackground(Color.BLUE);
+    	 *
+    	 */ 
             
-            // Sets Frame position to always start centered
-            this.setLocationRelativeTo(null);
-            
-            // closes entire program upon exit
-            this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            this.setTitle("Search Engine");
-            
-            //JPanel set up. Sets a panel to add components to.
+        //JPanel set up. Sets a panel to add components to.
             JPanel pan = new JPanel();
             pan.setLayout(null);
             
-            // Header layout
+        // Header layout
             JLabel Header = new JLabel("");
             Header.setText("Search Engine");
             Header.setFont(new Font("Serif", Font.BOLD, 30));
-            Header.setBounds(300, -60, 300, 200);
+            Header.setBounds(300, -50, 300, 200);
             
             pan.add(Header);
             
-            //Search button layout
+        // Menu Bar layout    
+            JMenuBar menubar = new JMenuBar();
+            this.setJMenuBar(menubar);
+            
+            JMenu file = new JMenu("File");
+            menubar.add(file);
+            
+            JMenu edit = new JMenu("Edit");
+            menubar.add(edit);
+            
+            JMenu maintenance = new JMenu("Maintnance");
+            menubar.add(maintenance);
+            
+            JMenu about = new JMenu("About");
+            menubar.add(about);
+            
+            
+        //Search button layout
             JButton searchBtn = new JButton("Search");
             searchBtn.setText("Search");
             searchBtn.setToolTipText("Search function");
-            searchBtn.setBounds(355, 150, 75, 25);
+            searchBtn.setBounds(355, 125, 75, 25);
             
             pan.add(searchBtn);
             
-            // Maintenance button layout
-            JButton maintBtn = new JButton("Search");
-            maintBtn.setText("Maintenance");
-            maintBtn.setToolTipText("Maintenance function");
-            maintBtn.setBounds(50, 600, 110, 25);
+        /* This button was replaced with a menu bar.
+         * It is being kept in case someone else wants to use it.
+         * Maintenance button layout
+         *   JButton maintBtn = new JButton();
+         *   maintBtn.setText("Maintenance");
+         *   maintBtn.setToolTipText("Maintenance function");
+         *   maintBtn.setBounds(50, 590, 110, 25);
+         *   
+         *   pan.add(maintBtn);
+         */ 
             
-            pan.add(maintBtn);
+        /* This button was replaced with a menu bar.
+         * It is being kept in case someone else wants to use it.
+         * About button layout
+         *    JButton aboutBtn = new JButton();
+         *    aboutBtn.setText("About");
+         *    aboutBtn.setToolTipText("Displays information about the Search Engine");
+         *    aboutBtn.setBounds(625, 590, 110, 25);
+         *    
+         *    pan.add(aboutBtn);
+         */
             
-            //Text Box layout
+        //Text Box layout
             JTextField textBox = new JTextField("Type here", 15);
-            textBox.setColumns(10);
+            textBox.setColumns(15);
             textBox.setText("");
             textBox.setToolTipText("Enter Search Terms here");
-            textBox.setBounds(245, 100, 300, 25);
+            textBox.setBounds(190, 80, 400, 25);
             
             pan.add(textBox);
-           
+            
+        //Radio Button layout  
+            JRadioButton allSearch = new JRadioButton("All Search Terms", true);
+            allSearch.setBounds(250, 180, 125, 25);
+            
+            this.add(allSearch);
+            
+            
+            JRadioButton exact = new JRadioButton("Exact Phrase", false);
+            exact.setBounds(410, 180, 125, 25);
+            
+            this.add(exact);
+            
+            group = new ButtonGroup();
+            group.add(allSearch);
+            group.add(exact);
+            
             this.add(pan);
-           
-            //This displays the GUI and all components (Buttons, text area, ect.)
+        //This displays the GUI and all components (Buttons, text area, ect.)
             this.setVisible(true);
     }
 
 }
+
